cc617519621c31a69868e255c33a7c3827ea3974
simon <1071939594@qq.com>
2021-04-14 10:14:05 +0800

    1

--- LU.cpp
+++ LU.cpp
@@ -35,7 +35,12 @@ int main()
         for (int i = k + 1; i < N; i++)
         {
             for (int j = k + 1; j < N; j++)
+            {
                 a[i][j] -= a[i][k] * a[k][j];
+                cout << endl;
+                printA(a);
+            }
+
             a[i][k] = 0;
         }
     }
